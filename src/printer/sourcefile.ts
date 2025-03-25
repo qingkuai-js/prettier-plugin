32617af6babe71698d6b4d742e5cb110dffcf16c
mlgq <ggqliang@gmail.com>
2025-03-25 15:48:51 +0800

    chore: do not use prettier debug mode

--- src/printer/sourcefile.ts
+++ src/printer/sourcefile.ts
@@ -25,9 +25,9 @@ import { util as qingkuaiCompilerUtil, util } from "qingkuai/compiler"
 const { hardline, line, fill, join, indent, softline, group, breakParent, ifBreak } = doc.builders
 
 export function embed(path: AstPath, _options: Options): EmbedReturnValue {
-    if (!process.env.PRETTIER_DEBUG) {
-        process.env.PRETTIER_DEBUG = "1"
-    }
+    // if (!process.env.PRETTIER_DEBUG) {
+    //     process.env.PRETTIER_DEBUG = "1"
+    // }
 
     const node: TemplateNode = path.getNode()
 
