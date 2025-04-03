7b866fd50708aa80f499ee45a2adde9dc83310d5
mlgq <ggqliang@gmail.com>
2025-04-03 15:09:43 +0800

    chore: update dependencies

--- src/parser.ts
+++ src/parser.ts
@@ -38,6 +38,7 @@ export function parse(text: string, options: ParserOptions) {
         children: [],
         attributes: [],
         componentTag: "",
+        pure: false,
         isEmbedded: false,
         lastChild: undefined,
         preWhiteSpace: false,
